3391e18327d2d0227b2e5d3756f9098059252d6f
Riken7 <rikenprajapati2005@gmail.com>
2024-04-27 12:55:10 +0530

    Priority Non-Preemptive

--- Priority_NPE.java
+++ Priority_NPE.java
@@ -1,5 +1,5 @@
 import java.util.*;
-//Low priority number means high priority
+//higher priority value means higher priority
 public class Priority_NPE {
     public static void main(String[] args) {
 
@@ -20,29 +20,28 @@ public class Priority_NPE {
         float arrivalTime = 0,finishTime= 0;
         float turnAroundTime = 0,waitingTime = 0;
         float avgTurnAroundTime = 0,avgWaitingTime = 0;
-
-        processes.sort(Comparator.comparingInt(p -> p.priority));
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("|PID\t|Arrival|Burst\t|Priority|Finish|Turn Around\t|Waiting\t|");
+        System.out.println("-------------------------------------------------------------------------");
+    
+        processes.sort(Comparator.comparingInt((Process p) -> p.arrivalTime).thenComparingInt(p -> p.priority));
         while(!processes.isEmpty()){
             Process current = processes.remove(0);
             if(current.arrivalTime > arrivalTime){
                 processes.add(current);
+                arrivalTime++;
             }
             else {
                 finishTime = arrivalTime + current.burstTime;
                 turnAroundTime = finishTime - current.arrivalTime;
                 waitingTime = turnAroundTime - current.burstTime;
-                System.out.println("---Process " + current.pid + "---");
-                System.out.println("Arrival Time : " + current.arrivalTime);
-                System.out.println("Burst Time : " + current.burstTime);
-                System.out.println("Finish Time : " + finishTime);
-                System.out.println("Turn Around Time : " + turnAroundTime);
-                System.out.println("Waiting Time : " + waitingTime + "\n");
-
+                System.out.println("|"+current.pid + "\t|" + current.arrivalTime + "\t|" + current.burstTime + "\t|"+ current.priority + "\t  |" + finishTime + "\t|" + turnAroundTime + "\t\t|" + waitingTime + "\t\t|");
                 avgTurnAroundTime += turnAroundTime; 
                 avgWaitingTime += waitingTime; 
 
                 arrivalTime = finishTime;
-                processes.sort(Comparator.comparingInt(p -> p.priority));
+                processes.sort(Comparator.comparingInt((Process p) -> p.arrivalTime).thenComparingInt(p -> p.priority));
+                
             }
         }
         avgTurnAroundTime /= size;
