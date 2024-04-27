4b67d74cc6e8c79c7afc8593195692622e5221af
Riken7 <rikenprajapati2005@gmail.com>
2024-04-27 12:55:23 +0530

    Round Robin

--- RR.java
+++ RR.java
@@ -1,27 +1,31 @@
 import java.util.*;
 public class RR {
     public static void main(String[] args) {
-    List<Process> processes = new ArrayList<>();
-    // processes.add(new Process(1,0,6));
-    // processes.add(new Process(2,1,2));
-    // processes.add(new Process(3,1,3));
-    // processes.add(new Process(4,2,1));
-    // processes.add(new Process(5,2,2));
+    System.out.println("Enter the number of processes : ");
+    Scanner sc = new Scanner(System.in);
+    int size = sc.nextInt();
 
-    processes.add(new Process(1, 0, 10));
-    processes.add(new Process(2, 1, 6));
-    processes.add(new Process(3, 3, 2));
-    processes.add(new Process(4, 5, 4));
-    // processes.add(new Process(5, 2, 2));
-    // processes.add(new Process(6, 6, 3));
-    int qt = 4 , csgo = 1; 
-    int size = processes.size();
+    List<Process> processes = new ArrayList<>(size);
+    for(int i = 0 ; i < size ; i++){
+        System.out.println("Enter the arrival time and burst time of process " + (i+1) + " : ");
+        int arrivalTime = sc.nextInt();
+        int burstTime = sc.nextInt();
+        processes.add(new Process(i+1, arrivalTime, burstTime));
+    }
+    System.out.println("Enter the quantum time : ");
+    int qt = sc.nextInt();
+    System.out.println("Enter the context switch time : ");
+    int csgo = sc.nextInt();
+    sc.close();
     float finishTime = 0 , currentTime = 0;
     float turnAroundTime = 0,waitingTime = 0;
     float avgTurnAroundTime = 0,avgWaitingTime = 0;
     processes.sort(Comparator.comparingInt(p -> p.arrivalTime));
         
     Queue<Process> queue = new LinkedList<>();
+    System.out.println("-----------------------------------------------------------------");
+    System.out.println("|PID\t|Arrival|Burst\t|Finish\t|Turn Around\t|Waiting\t|");
+    System.out.println("-----------------------------------------------------------------");
     while(!queue.isEmpty() || !processes.isEmpty()){
         while(!processes.isEmpty() && processes.get(0).arrivalTime <= currentTime){
             queue.add(processes.remove(0));
@@ -43,14 +47,8 @@ public class RR {
                     finishTime = currentTime;
                     
                     turnAroundTime = finishTime - current.arrivalTime;
-                    waitingTime = turnAroundTime - current.bTime; // since burstTime was changing so we stored it in bTime which is constant 
-                    System.out.println("---Process " + current.pid + "---");
-                    System.out.println("Arrival Time : " + current.arrivalTime);
-                    System.out.println("Burst Time : " + current.bTime);
-                    System.out.println("Finish Time : " + finishTime);
-                    System.out.println("Turn Around Time : " + turnAroundTime);
-                    System.out.println("Waiting Time : " + waitingTime + "\n");
-
+                    waitingTime = turnAroundTime - current.bTime;
+                    System.out.println("|"+current.pid + "\t|" + current.arrivalTime + "\t|" + current.bTime + "\t|" + finishTime + "\t|" + turnAroundTime + "\t\t|" + waitingTime + "\t\t|");
                     avgTurnAroundTime += turnAroundTime; 
                     avgWaitingTime += waitingTime; 
                     currentTime += csgo;
@@ -62,7 +60,7 @@ public class RR {
         avgTurnAroundTime /= size;
         avgWaitingTime /= size;
 
-        System.out.println("Average Turn Around Time : " + avgTurnAroundTime + "\nAverage Waiting Time : " + avgWaitingTime);
+        System.out.println("\nAverage Turn Around Time : " + avgTurnAroundTime + "\nAverage Waiting Time : " + avgWaitingTime);
 
 }
     
