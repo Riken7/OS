cc2b66ac2ff47dcdbf67ddf64202ec0f05b6561b
Riken7 <rikenprajapati2005@gmail.com>
2024-05-12 11:56:25 +0530

    optimal algo added

--- PageReplacement.java
+++ PageReplacement.java
@@ -1,16 +1,43 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 public class PageReplacement {
 
     public static void main(String[] args) {
-        int[] pages = {4, 7 , 6 , 1 , 7 , 6 , 1  , 2 ,7 ,2 };
+        // int[] pages = {4, 7 , 6 , 1 , 7 , 6 , 1  , 2 ,7 ,2 };
         // int[] pages = {5 , 0 , 1 , 2 , 0 , 3 , 2 , 0 , 3 , 4 , 1 , 0 , 5 , 0 ,4,3,2,1,2,0,1 };
-        // int[] pages = {7,0,7,4,3,1,4,7,2,0,4,3,0,3,2,7};
-        int frames = 3;
-
-        // FIFO(pages, frames);
-        // Optimal(pages, frames);
-        LRU(pages, frames);
-        // LFU(pages, frames);
+        // int[] pages = {7,0,2,4,3,1,4,7,2,0,4,3,0,3,2,7};
+        // int[] pages = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
+        // int frames = 3;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of frames: ");
+        int frames = sc.nextInt();
+        System.out.println("Enter the number of pages: ");
+        int n = sc.nextInt();
+        int[] pages = new int[n];
+        System.out.println("Enter the pages: ");
+        for(int i=0 ; i<n ; i++){
+            pages[i] = sc.nextInt();
+        }
+        System.out.println("Select the page replacement algorithm: ");
+        System.out.print("1. FIFO \n2. Optimal \n3. LRU \n4. LFU \n");
+        int choice = sc.nextInt();
+        switch(choice){
+            case 1:
+                FIFO(pages, frames);
+                break;
+            case 2:
+                Optimal(pages, frames);
+                break;
+            case 3:
+                LRU(pages, frames);
+                break;
+            case 4:
+                LFU(pages, frames);
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
     }
     public static void FIFO(int[] pages , int frames){
         ArrayList<Integer> frameList = new ArrayList<>(frames);
@@ -20,7 +47,7 @@ public class PageReplacement {
         int i=0;
         System.out.println("Page Replacement Algorithm: FIFO");
         System.out.println("-----------------------------------");
-        System.out.println("Frame List\t\tHit/Miss");
+        System.out.println("F1  F2  F3\t\tHit/Miss");
         System.out.println("-----------------------------------");
 
         while(frameList.size()!=frames){
@@ -34,10 +61,10 @@ public class PageReplacement {
             }
 
             for(int j=0;j<frameList.size();j++){
-                System.out.print(frameList.get(j)+"  ");
+                System.out.print(frameList.get(j)+"   ");
             }
             for(int j=0;j<frames-frameList.size();j++){
-                System.out.print("X  ");
+                System.out.print("X   ");
             }
             System.out.println("\t\t" + hitOrMissList.get(i));
             i++;
@@ -55,7 +82,7 @@ public class PageReplacement {
                 pointer = (pointer+1)%frames;
             }
             for(int j=0;j<frameList.size();j++){
-                System.out.print(frameList.get(j)+"  ");
+                System.out.print(frameList.get(j)+"   ");
             }
             
             System.out.println("\t\t" + hitOrMissList.get(i));
@@ -76,7 +103,7 @@ public class PageReplacement {
         int pageFaults = 0;
         int pageHit = 0;
         int i=0;
-        System.out.println("Page Replacement Algorithm: FIFO");
+        System.out.println("Page Replacement Algorithm: Optimal");
         System.out.println("-----------------------------------");
         System.out.println("Frame List\t\tHit/Miss");
         System.out.println("-----------------------------------");
@@ -89,7 +116,8 @@ public class PageReplacement {
                 frameList.add(pages[i]);
                 hitOrMissList.add("Miss");
                 pageFaults++;
-            }for(int j=0;j<frameList.size();j++){
+            }
+            for(int j=0;j<frameList.size();j++){
                 System.out.print(frameList.get(j)+"  ");
             }
             for(int j=0;j<frames-frameList.size();j++){
@@ -99,30 +127,36 @@ public class PageReplacement {
             i++;
         }
         int pointer = 0;
-        int j=i;
-        int breakPoint = 0;
+        
         for(;i<pages.length;i++){
+            int breakPoint = 0;
+            int j=i;
             if(frameList.contains(pages[i])){
                 hitOrMissList.add("Hit");
                 pageHit++;
             }else{
+                ArrayList<Integer> temp = new ArrayList<>();
                 while(j<pages.length){
-                    if(frameList.contains(pages[j])){
-                        pointer = frameList.indexOf(pages[j]);
-                        breakPoint++;
-                        pointer = (pointer+1)%frames;
+                    if(frameList.contains(pages[j]) && !temp.contains(pages[j])){
+                        temp.add(pages[j]);
+                        ++breakPoint;
                         if(breakPoint==frames-1){
                             break;
                         }
                     }
                     j++;
                 }
+            
+                for(int k=0;k<frameList.size();k++){
+                    if(!temp.contains(frameList.get(k))){
+                        pointer = k;
+                        break;
+                    }
+                }
                 hitOrMissList.add("Miss");
                 pageFaults++;
                 frameList.remove(pointer);
                 frameList.add(pointer,pages[i]);
-                pointer = (pointer+1)%frames;
-
             }
             for(int k=0;k<frameList.size();k++){
                 System.out.print(frameList.get(k)+"  ");
@@ -146,7 +180,7 @@ public class PageReplacement {
         int pageFaults = 0;
         int pageHit = 0;
         int i=0;
-        System.out.println("Page Replacement Algorithm: FIFO");
+        System.out.println("Page Replacement Algorithm: LRU");
         System.out.println("-----------------------------------");
         System.out.println("Frame List\t\tHit/Miss");
         System.out.println("-----------------------------------");
@@ -207,22 +241,23 @@ public class PageReplacement {
     public static void LFU(int[] pages, int frames){
         ArrayList<Integer> frameList = new ArrayList<>(frames);
         ArrayList<String> hitOrMissList = new ArrayList<>();
-        // ArrayList<Integer> freq = new ArrayList<>(frames);
-        ArrayList<ArrayList<Integer>> freq = new ArrayList<>(frames);
+        ArrayList<ArrayList<Integer>> frequency = new ArrayList<>();
         int pageFaults = 0;
         int pageHit = 0;
         int i=0;
-        System.out.println("Page Replacement Algorithm: FIFO");
+        System.out.println("Page Replacement Algorithm: LFU");
         System.out.println("-----------------------------------");
         System.out.println("Frame List\t\tHit/Miss");
         System.out.println("-----------------------------------");
         while (frameList.size()!=frames){
-            frameList.add(pages[i]);
-            hitOrMissList.add("Miss");
-            pageFaults++;
-            ArrayList<Integer> temp = new ArrayList<>();
-            temp.add(pages[i]);
-            freq.add(temp);
+            if(frameList.contains(pages[i])) {
+                hitOrMissList.add("Hit");
+                pageHit++;
+            } else {
+                frameList.add(pages[i]);
+                hitOrMissList.add("Miss");
+                pageFaults++;
+            }
             for(int j=0;j<frameList.size();j++){
                 System.out.print(frameList.get(j)+"  ");
             }
@@ -232,6 +267,5 @@ public class PageReplacement {
             System.out.println("\t\t" + hitOrMissList.get(i));
             i++;
         } 
-        System.out.println(freq);
     }
 }
